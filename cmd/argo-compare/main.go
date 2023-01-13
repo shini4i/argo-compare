123d59ac4a1744e91d8a31c9e0b92aa323a68624
Vadim Gedz <vadims.gedzs@linux-tech.org>
2023-01-14 01:23:18 +0200

    chore: add drop cache support

--- cmd/argo-compare/main.go
+++ cmd/argo-compare/main.go
@@ -35,17 +35,29 @@ var (
 )
 
 var CLI struct {
-	Debug   bool             `help:"Enable debug mode" short:"d"`
-	Version kong.VersionFlag `help:"Show version" short:"v"`
+	Debug     bool             `help:"Enable debug mode" short:"d"`
+	DropCache Cache            `help:"Drop cache directory"`
+	Version   kong.VersionFlag `help:"Show version" short:"v"`
 
 	Branch struct {
 		Name               string `arg:"" type:"string"`
 		File               string `help:"Compare a single file" short:"f"`
-		PreserveHelmLabels bool   `help:"Preserve Helm labels"`
+		PreserveHelmLabels bool   `help:"Preserve Helm labels during comparison"`
 	} `cmd:"" help:"target branch to compare with" type:"string"`
 }
 
 type execContext = func(name string, arg ...string) *exec.Cmd
+
+type Cache bool
+
+func (c *Cache) BeforeApply(app *kong.Kong) error {
+	err := os.RemoveAll(cacheDir)
+	if err != nil {
+		app.Exit(1)
+	}
+	app.Exit(0)
+	return nil
+}
 
 func loggingInit(level logging.Level) {
 	backend := logging.NewLogBackend(os.Stdout, "", 0)
--- internal/models/application.go
+++ internal/models/application.go
@@ -1,29 +1,21 @@
 package models
 
-type Metadata struct {
-	Name      string `yaml:"name"`
-	Namespace string `yaml:"namespace"`
-}
-
-type Helm struct {
-	Values     string   `yaml:"values,omitempty"`
-	ValueFiles []string `yaml:"valueFiles,omitempty"`
-	Path       string   `yaml:"path,omitempty"`
-}
-
-type Source struct {
-	RepoURL        string `yaml:"repoURL"`
-	Chart          string `yaml:"chart,omitempty"`
-	TargetRevision string `yaml:"targetRevision"`
-	Helm           Helm   `yaml:"helm"`
-}
-
-type Spec struct {
-	Source Source `yaml:"source"`
-}
-
 type Application struct {
-	Kind     string   `yaml:"kind"`
-	Metadata Metadata `yaml:"metadata"`
-	Spec     Spec     `yaml:"spec"`
+	Kind     string `yaml:"kind"`
+	Metadata struct {
+		Name      string `yaml:"name"`
+		Namespace string `yaml:"namespace"`
+	} `yaml:"metadata"`
+	Spec struct {
+		Source struct {
+			RepoURL        string `yaml:"repoURL"`
+			Chart          string `yaml:"chart,omitempty"`
+			TargetRevision string `yaml:"targetRevision"`
+			Path           string `yaml:"path,omitempty"`
+			Helm           struct {
+				Values     string   `yaml:"values,omitempty"`
+				ValueFiles []string `yaml:"valueFiles,omitempty"`
+			} `yaml:"helm"`
+		} `yaml:"source"`
+	} `yaml:"spec"`
 }
