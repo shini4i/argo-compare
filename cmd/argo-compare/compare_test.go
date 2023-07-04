9bc6cb4b0c3a2ddb72b5947192abfa391c9bca5f
Vadim Gedz <vadims@linux-tech.io>
2023-07-05 01:30:28 +0300

    chore: add test implementation of TestFindAndStripHelmLabels

--- cmd/argo-compare/compare_test.go
+++ cmd/argo-compare/compare_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"os"
 	"reflect"
 	"testing"
 )
@@ -51,4 +53,66 @@ func TestGenerateFilesStatus(t *testing.T) {
 		fmt.Println(c.diffFiles)
 		t.Errorf("generateFilesStatus() did not generate expected diffFiles")
 	}
+}
+
+func TestFindAndStripHelmLabels(t *testing.T) {
+	// Prepare test data
+	testFile := "../../testdata/deployment.yaml"
+	backupFile := testFile + ".bak"
+
+	// Read the original file content
+	originalData, err := os.ReadFile(testFile)
+	if err != nil {
+		t.Fatalf("Failed to read the original file: %s", err)
+	}
+
+	// Create a backup of the original file
+	if err := os.WriteFile(backupFile, originalData, 0644); err != nil {
+		t.Fatalf("Failed to create a backup of the test file: %s", err)
+	}
+	defer func() {
+		// Restore the original file
+		err := os.Rename(backupFile, testFile)
+		if err != nil {
+			t.Fatalf("Failed to restore the test file: %s", err)
+		}
+	}()
+
+	// Change directory to the testdata directory
+	if err := os.Chdir("../../testdata"); err != nil {
+		t.Fatalf("Failed to change directory: %s", err)
+	}
+
+	// Create an instance of Compare
+	c := &Compare{}
+
+	// Call the method to find and strip Helm labels
+	c.findAndStripHelmLabels()
+
+	// Return to the original directory
+	if err := os.Chdir("../cmd/argo-compare"); err != nil {
+		t.Fatalf("Failed to change directory: %s", err)
+	}
+
+	// Read the modified file
+	modifiedData, err := os.ReadFile(testFile)
+	if err != nil {
+		t.Fatalf("Failed to read the modified file: %s", err)
+	}
+
+	// Define the expected modified content
+	expectedOutput := `# for testing purpose we need only limited fields
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    app.kubernetes.io/instance: traefik-web
+    app.kubernetes.io/name: traefik
+    argocd.argoproj.io/instance: traefik
+  name: traefik
+  namespace: web
+`
+
+	// Compare the modified output with the expected output
+	assert.Equal(t, expectedOutput, string(modifiedData))
 }
