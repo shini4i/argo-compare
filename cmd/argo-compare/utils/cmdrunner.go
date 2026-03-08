2183aa6fdc3836f9647e355f9a5c4205e6eb1ad2
Vadim Gedz <github@shini4i.dev>
2026-03-08 16:53:49 +0200

    chore: add gosec exclude

--- cmd/argo-compare/utils/cmdrunner.go
+++ cmd/argo-compare/utils/cmdrunner.go
@@ -14,7 +14,7 @@ type RealCmdRunner struct{}
 // Run executes cmd with args and captures stdout and stderr strings.
 // The context can be used to cancel the command or set a timeout.
 func (r *RealCmdRunner) Run(ctx context.Context, cmd string, args ...string) (string, string, error) {
-	command := exec.CommandContext(ctx, cmd, args...)
+	command := exec.CommandContext(ctx, cmd, args...) // #nosec G204 -- cmd is always a hardcoded binary name from internal callers
 
 	var stdoutBuffer, stderrBuffer bytes.Buffer
 	command.Stdout = &stdoutBuffer
